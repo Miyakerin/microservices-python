45c931994ffa959e0ee2d8a02e286e691effd62b
Ruslan <ruslanishmaev03@gmail.com>
2024-08-13 00:21:23 +0300

    changes in behabiour of controller

--- src/recipes/controllers/recipe_controller.py
+++ src/recipes/controllers/recipe_controller.py
@@ -5,21 +5,21 @@ from src.recipes.dto.recipe_dto import RecipeDto
 router = APIRouter(prefix="/api/recipes", tags=["Recipes"])
 
 
-@router.get("/")
+@router.get("")
 def get_recipes() -> list[RecipeDto]:
     return recipe_services.get_recipes()
 
 
-@router.get("/{recipe_id}/")
+@router.get("/{recipe_id}")
 def get_recipe(recipe_id: int) -> RecipeDto:
     return recipe_services.get_recipe(recipe_id)
 
 
-@router.post("/")
+@router.post("")
 def create_recipe(title: str, description: str) -> bool:
     return recipe_services.post_recipe(title, description)
 
 
-@router.delete("/{recipe_id}/")
+@router.delete("/{recipe_id}")
 def delete_recipe(recipe_id: int) -> bool:
     return recipe_services.delete_recipe(recipe_id)
