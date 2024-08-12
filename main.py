210dbb48715febd0457cfe8de0c739ddec1b25a6
Ruslan <ruslanishmaev03@gmail.com>
2024-08-12 22:40:44 +0300

    lifespan for app

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 import src.recipes
 from src.core.config import settings
+from src.recipes.services.recipe_services import engine
+from src.recipes.entities.base import Base
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(src.recipes.controllers.recipe_controller.router)
 
 
