d3867fb028b84f4a0dcc9cca4531481f50a2073d
Ruslan <ruslanishmaev03@gmail.com>
2024-08-13 13:41:05 +0300

    delete __pycache__

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from src.recipes.entities.base import Base
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    Base.metadata.create_all(engine)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
--- src/dbUtil/db_helper.py
+++ src/dbUtil/db_helper.py
@@ -1,14 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
 class DatabaseHelper:
     def __init__(self, db_setting):
         self.connection_string = (
-            f"postgresql+psycopg2://{db_setting.user}:{db_setting.password}"
+            f"postgresql+asyncpg://{db_setting.user}:{db_setting.password}"
             f"@{db_setting.host}:{db_setting.port_host}/{db_setting.db}"
         )
-        self.engine = create_engine(self.connection_string, echo=db_setting.echo)
-        self.session_factory = sessionmaker(
+        self.engine = create_async_engine(self.connection_string, echo=db_setting.echo)
+        self.session_factory = async_sessionmaker(
             bind=self.engine, autocommit=False, autoflush=False, expire_on_commit=False
         )
