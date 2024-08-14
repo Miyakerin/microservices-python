a4586551b75a29bb22662ca43ccabbf726be4cda
Ruslan <ruslanishmaev03@gmail.com>
2024-08-14 15:46:45 +0300

    connecting to eureka and getting auth public key on startup

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 from contextlib import asynccontextmanager
 
 import uvicorn
@@ -7,14 +9,44 @@ import src.recipes
 from src.core.config import settings
 from src.recipes.controllers.recipe_controller import db_help
 from src.recipes.entities.base import Base
+import py_eureka_client.eureka_client as eureka_client
+
+logger = logging.getLogger("uvicorn.error")
 
 engine = db_help.engine
+
+
+async def get_public_key(interval: int = 1800):
+    # auth_url = f"http://{settings.auth_settings.domain_name}/api/auth/jwt/publicKey"
+    while True:
+        # logger.info(f"Getting public key at {auth_url}")
+        ans = await eureka_client.do_service_async(
+            settings.auth_settings.domain_name, "/api/auth/jwt/publicKey"
+        )
+        settings.auth_settings.public_key = ans
+        logger.info(
+            f"Getting new public key from: {settings.auth_settings.domain_name}"
+        )
+
+        await asyncio.sleep(interval)
+
+
+async def connect_to_eureka():
+    eureka_url = f"http://{settings.eureka_settings.host}:{settings.eureka_settings.port}/eureka/"
+    logger.info(f"Connecting to eureka at {eureka_url}")
+    await eureka_client.init_async(
+        eureka_server=eureka_url,
+        app_name=settings.eureka_settings.app_name,
+        instance_port=settings.fastapi_settings.port,
+    )
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    await connect_to_eureka()
+    asyncio.create_task(get_public_key(interval=1800))
     yield
 
 
--- src/core/config.py
+++ src/core/config.py
@@ -18,9 +18,24 @@ class FastAPISettings(BaseSettings):
     port: int = 8080
 
 
+class EurekaSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="EUREKA_", extra="ignore")
+    host: str = "localhost"
+    port: int = 8081
+    app_name: str = "app_name"
+
+
+class AuthSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="AUTHSERVICE_", extra="ignore")
+    domain_name: str = "domain_name"
+    public_key: str = "public_key"
+
+
 class Settings(BaseSettings):
     recipes_db: RecipesDbSettings = RecipesDbSettings()
     fastapi_settings: FastAPISettings = FastAPISettings()
+    eureka_settings: EurekaSettings = EurekaSettings()
+    auth_settings: AuthSettings = AuthSettings()
 
 
 settings = Settings()
